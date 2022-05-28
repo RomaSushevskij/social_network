ae2dcffdb55ae77c63c1204f1130370beddae5db
RomaSushevskij <roma.sushevskij@yandex.ru>
2022-05-28 05:18:53 +0300

    lessons 84-86 (useState, useEffect, virtual DOM)

---
+++ src/components/Profile/ProfileStatusHooks/ProfileStatusHooks.tsx
@@ -0,0 +1,31 @@
+import React, {useEffect, useState} from "react";
+import styleModule from './ProfileStatusHooks.module.css';
+import EditableSpan from "../../generic/EditableSpan/EditableSpan";
+
+type ProfileStatusPropsType = {
+    status: string
+    updateStatus: (status: string) => void
+}
+
+export const ProfileStatusHooks = ({status, updateStatus}: ProfileStatusPropsType) => {
+    const [localStatus, setLocalStatus] = useState(status);
+    const onBlurEnterCallback = () => {
+        updateStatus(localStatus)
+    }
+    useEffect(() => {
+        setLocalStatus(status)
+    }, [status])
+    return (
+        <div className={styleModule.profileStatus}>
+            <EditableSpan
+                value={localStatus}
+                onChangeText={setLocalStatus}
+                spanProps={{children: status ? undefined : 'enter status...'}}
+                onBlur={onBlurEnterCallback}
+                onEnter={onBlurEnterCallback}/>
+        </div>
+    )
+
+
+};
+
