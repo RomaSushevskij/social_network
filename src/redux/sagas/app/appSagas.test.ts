48f36299d8af5bf5106cbfcd4203bc297f5bb6e5
RomaSushevskij <roma.sushevskij@yandex.ru>
2022-10-15 13:03:58 +0300

    test for error initializeAppWorkerSage

--- src/redux/sagas/app/appSagas.test.ts
+++ src/redux/sagas/app/appSagas.test.ts
@@ -1,7 +1,7 @@
 import {initializeAppWorkerSage} from "./appSagas";
 import {getAuthorizationInfoWorkerSaga} from "../auth/authSagas";
 import {put} from "redux-saga/effects";
-import {setAppInitializeValue} from "../../redusers/app/appReducer";
+import {setAppError, setAppInitializeValue} from "../../redusers/app/appReducer";
 
 test('initialize app success', () => {
     const gen = initializeAppWorkerSage();
@@ -9,3 +9,10 @@ test('initialize app success', () => {
     expect(gen.next().value).toEqual(getAuthorizationInfoWorkerSaga());
     expect(gen.next().value).toEqual(put(setAppInitializeValue(true)))
 });
+
+test('initialize app with error', () => {
+    const gen = initializeAppWorkerSage();
+    const error = {message: 'some error'}
+    expect(gen.next().value).toEqual(getAuthorizationInfoWorkerSaga());
+    expect(gen.throw(error).value).toEqual(put(setAppError(error.message)))
+})
