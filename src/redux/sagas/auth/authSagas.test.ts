2fc45b2deacc41c6c896b3550fdb17eec3f3aabf
RomaSushevskij <roma.sushevskij@yandex.ru>
2022-10-15 13:53:21 +0300

    test for getAuthorizationInfoWorkerSaga error response code

--- src/redux/sagas/auth/authSagas.test.ts
+++ src/redux/sagas/auth/authSagas.test.ts
@@ -4,6 +4,7 @@ import {call, put, select} from "redux-saga/effects";
 import {setAuthUserData, setFullNameAndAvatar} from "../../redusers/auth/authReducer";
 import {setProfile} from "../../redusers/profileReducer/profileReducer";
 import {getFollowersWorkerSaga} from "../profile/profileSagas";
+import {setAppError} from "../../redusers/app/appReducer";
 
 
 test('getAuthorizationInfoWorkerSaga success response code', () => {
@@ -47,4 +48,17 @@ test('getAuthorizationInfoWorkerSaga success response code', () => {
     expect(gen.next(profileData).value).toEqual(put(setFullNameAndAvatar(fullName, photos.small)));
     expect(gen.next().value).toEqual(put(setProfile(profileData)));
     expect(gen.next().value).toEqual(getFollowersWorkerSaga().next().value)
+})
+
+test('getAuthorizationInfoWorkerSaga error response code', () => {
+    const gen = getAuthorizationInfoWorkerSaga();
+    const data: GetAuthUserDataType = {
+        data: {id: 12, email: '', login: ''},
+        fieldsErrors: [],
+        messages: ['some error'],
+        resultCode: RESPONSE_RESULT_CODES.error
+    };
+    expect(gen.next().value).toEqual(call(authMeAPI.getAuthorizationInfo));
+    // @ts-ignore
+    expect(gen.next(data).value).toEqual(put(setAppError(data.messages[0])))
 })
