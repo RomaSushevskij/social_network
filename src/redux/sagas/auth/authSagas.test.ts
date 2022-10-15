88ac6f0e99df228be92c8c6faed87c0855157aae
RomaSushevskij <roma.sushevskij@yandex.ru>
2022-10-15 13:50:21 +0300

    test for getAuthorizationInfoWorkerSaga success response code

---
+++ src/redux/sagas/auth/authSagas.test.ts
@@ -0,0 +1,50 @@
+import {getAuthorizationInfoWorkerSaga} from "./authSagas";
+import {authMeAPI, GetAuthUserDataType, GetProfileDataType, profileAPI, RESPONSE_RESULT_CODES} from "../../../api/api";
+import {call, put, select} from "redux-saga/effects";
+import {setAuthUserData, setFullNameAndAvatar} from "../../redusers/auth/authReducer";
+import {setProfile} from "../../redusers/profileReducer/profileReducer";
+import {getFollowersWorkerSaga} from "../profile/profileSagas";
+
+
+test('getAuthorizationInfoWorkerSaga success response code', () => {
+    const gen = getAuthorizationInfoWorkerSaga();
+    const data: GetAuthUserDataType = {
+        data: {id: 12, email: '', login: ''},
+        fieldsErrors: [],
+        messages: [],
+        resultCode: RESPONSE_RESULT_CODES.success
+    };
+    const profileData: GetProfileDataType = {
+        aboutMe: 'aboutMe',
+        contacts: {
+            facebook: 'facebook',
+            website: 'website',
+            vk: 'vk',
+            twitter: 'twitter',
+            instagram: 'instagram',
+            youtube: 'youtube',
+            github: 'github',
+            mainLink: 'mainLink',
+        },
+        lookingForAJob: true,
+        lookingForAJobDescription: 'lookingForAJobDescription',
+        fullName: 'fullName',
+        userId: 12,
+        photos: {
+            small: 'small',
+            large: 'large',
+        },
+    }
+    const state = {auth: {id: data.data.id}, profilePage: {profile: {fullName: 'fullName', photos: {small: 'small'}},}};
+    const {fullName, photos} = state.profilePage.profile;
+    expect(gen.next().value).toEqual(call(authMeAPI.getAuthorizationInfo));
+    // @ts-ignore
+    expect(gen.next(data).value).toEqual(put(setAuthUserData(data.data, true)));
+    expect(gen.next().value).toEqual(select());
+    // @ts-ignore
+    expect(gen.next(state).value).toEqual(call(profileAPI.getProfile, data.data.id));
+    // @ts-ignore
+    expect(gen.next(profileData).value).toEqual(put(setFullNameAndAvatar(fullName, photos.small)));
+    expect(gen.next().value).toEqual(put(setProfile(profileData)));
+    expect(gen.next().value).toEqual(getFollowersWorkerSaga().next().value)
+})
--- src/redux/sagas/auth/authSagas.ts
+++ src/redux/sagas/auth/authSagas.ts
@@ -17,22 +17,21 @@ export function* getAuthorizationInfoWorkerSaga() {
         const data: GetAuthUserDataType = yield call(authMeAPI.getAuthorizationInfo);
         if (data.resultCode === RESPONSE_RESULT_CODES.success) {
             yield put(setAuthUserData(data.data, true))
+            const state: AppStateType = yield select();
+            const id = getAuthUserIDSelector(state);
+            if (id) {
+                const profileData: GetProfileDataType = yield call(profileAPI.getProfile, id);
+                const fullName = profileData?.fullName
+                const avatar = profileData?.photos.small
+                yield put(setFullNameAndAvatar(fullName, avatar))
+                yield put(setProfile(profileData))
+            }
+            yield* getFollowersWorkerSaga();
         } else {
             if (data.messages.length) {
                 yield put(setAppError(data.messages[0]));
             }
-
         }
-        const state: AppStateType = yield select();
-        const id = getAuthUserIDSelector(state);
-        if (id) {
-            const profileData: GetProfileDataType = yield call(profileAPI.getProfile, id);
-            const fullName = profileData?.fullName
-            const avatar = profileData?.photos.small
-            yield put(setFullNameAndAvatar(fullName, avatar))
-            yield put(setProfile(profileData))
-        }
-        yield* getFollowersWorkerSaga();
     } catch (e) {
         const error = e as AxiosError;
         yield put(setAppError(error.message));
