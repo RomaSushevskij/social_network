1a65270329052cc7dc696998819f4c4d20aaba17
RomaSushevskij <roma.sushevskij@yandex.ru>
2022-10-16 11:52:41 +0300

    test: getNewsWorkerSaga should be with searching error (no-matches)

--- src/redux/sagas/news/newsSagas.test.ts
+++ src/redux/sagas/news/newsSagas.test.ts
@@ -103,7 +103,60 @@ test('getNewsWorkerSaga should be with searching error (error)', () => {
         page_size: action.payload.page_size,
         page: action.payload.page
     }));
+    // @ts-ignore
     expect(newsGen.next(newsData).value).toEqual(put(setAppError(newsData.error_code + ', ' + newsData.message)));
+    expect(newsGen.next().value).toEqual(put(setNews([])));
+    expect(newsGen.next().value).toEqual(put(setNewsTotal(0)));
+    expect(newsGen.next().value).toEqual(put(setIsNewsLoading(false)));
+})
+
+test('getNewsWorkerSaga should be with searching error (no-matches)', () => {
+    const action: ReturnType<typeof getNews> = {
+        type: newsActions.GET_NEWS,
+        payload: {page_size: 10, page: 1}
+    };
+    const searchString = 'searchString';
+    const newsData: GetNewsDataType = {
+        articles: [{
+            author: 'author',
+            clean_url: 'clean_url',
+            language: 'language',
+            link: 'link',
+            media: 'media',
+            published_date: 'published_date',
+            summary: 'summary',
+            title: 'title',
+            topic: 'topic',
+            _id: '_id',
+            _score: 10,
+        }],
+        page: 1,
+        page_size: 10,
+        status: NEWS_RESULT_CODES.no_matches,
+        error_code: 'error_code',
+        message: 'message',
+        total_hits: 100,
+        total_pages: 10,
+        user_input: {
+            from: 'from',
+            page: 1,
+            q: searchString,
+            size: 10,
+            sort_by: "relevancy",
+        }
+    }
+    const newsGen = getNewsWorkerSaga(action);
+
+    expect(newsGen.next().value).toEqual(select(selectNewsSearchValue));
+    // @ts-ignore
+    expect(newsGen.next(searchString).value).toEqual(put(setIsNewsLoading(true)));
+    expect(newsGen.next().value).toEqual(call(newsAPI.getNews, {
+        q: searchString,
+        page_size: action.payload.page_size,
+        page: action.payload.page
+    }));
+    // @ts-ignore
+    expect(newsGen.next(newsData).value).toEqual(put(setAppError(newsData.status)));
     expect(newsGen.next().value).toEqual(put(setNews([])));
     expect(newsGen.next().value).toEqual(put(setNewsTotal(0)));
     expect(newsGen.next().value).toEqual(put(setIsNewsLoading(false)));
